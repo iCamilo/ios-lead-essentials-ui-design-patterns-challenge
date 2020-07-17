a1213d9cb09d75aed9d6797b9901f2c132665f89
Iván Fuertes <ivancamilofuerte>
2020-07-17 13:48:19 -0500

    Place  MVVM `ErrorView` directly in the `Feed` storyboard and remove the xib, as is not part of the scope

--- MVVM Module/Source/Feed UI/Views/ErrorView.swift
+++ MVVM Module/Source/Feed UI/Views/ErrorView.swift
@@ -4,7 +4,6 @@
 
 import UIKit
 
-@IBDesignable
 public final class ErrorView: UIView {
 	@IBOutlet private var label: UILabel!    
     @IBOutlet private var container: UIView!
@@ -16,17 +15,7 @@ public final class ErrorView: UIView {
 	private var isVisible: Bool {
 		return alpha > 0
 	}
-	
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupFromNib()
-    }
-    
-    public required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupFromNib()
-    }
-    
+	    
 	public override func awakeFromNib() {
 		super.awakeFromNib()
 		
@@ -53,34 +42,8 @@ public final class ErrorView: UIView {
 	}
     
     private func addRounderBorderColor() {
-        container.layer.cornerRadius = 5
-        container.layer.borderWidth = 2
-        container.layer.borderColor = UIColor.cayenne.cgColor        
-    }
-}
-
-extension ErrorView {
-    static var nib: UINib {
-        let bundle = Bundle(for: Self.self)
-        return UINib(nibName: String(describing: Self.self), bundle: bundle)
-    }
-
-    func setupFromNib() {
-        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else {
-            return
-        }
-        
-        addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
-        view.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        view.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
-        view.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
-    }
-}
-
-extension UIColor {
-    static var cayenne: UIColor {
-        UIColor(red: 148/255, green: 17/255, blue: 0, alpha: 1.0)
+        container.layer.cornerRadius = 2
+        container.layer.borderWidth = 1
+        container.layer.borderColor = UIColor.red.cgColor
     }
 }
